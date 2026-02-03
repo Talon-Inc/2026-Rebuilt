d7c2c470d87eeedb9a1dea0f0151f3b187bf8b81
Jonathan Labrum <jonathan.d.labrum@gmail.com>
2026-02-03 17:54:32 -0600

    added configs file and base for shooter

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,6 +8,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 /**
  * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
@@ -27,5 +29,29 @@ public final class Constants {
 
     /** Replaying from a log file. */
     REPLAY
+  }
+    public static final class DriveConstants {
+    // Drive motors - SparkMax Controller
+    public static final int kFrontLeftDriveCanId = 1;
+    public static final int kRearLeftDriveCanId = 3;
+    public static final int kFrontRightDriveCanId = 2;
+    public static final int kRearRightDriveCanId = 4;
+    public static final MotorType kDriveMotorType = MotorType.kBrushed;
+    public static final double kSpeed = .5;
+    public static final IdleMode kDriveIdleMode = IdleMode.kBrake;
+    public static final int kDriveMotorCurrentLimit = 30;
+    public static double kDriveFactor = .5; // changed for turbo
+    public static double kTurnFactor = .5;
+    public static int kDriveReverse = 1; // Default drive; 1 = Front is panel, -1 = Front is intake
+  }
+
+  public static final class ShooterConstants {
+    // SparkMax Controllers
+    public static final int kLeftShooterCanId = 11;
+    public static final int kRightShooterCanId = 12;
+    public static final MotorType kShooterMotorType = MotorType.kBrushed;
+  }
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
   }
 }
