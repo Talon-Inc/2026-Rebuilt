d9bb4d50b29ea265295e49f71af5ca28495adeb4
JonathanLabrum <jonathan.d.labrum@gmail.com>
2026-03-07 15:21:57 -0600

    Update drive motors to spark max

--- src/main/java/frc/robot/subsystems/drive/ModuleIOSpark.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOSpark.java
@@ -23,7 +23,6 @@ import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.Debouncer;
@@ -94,7 +93,7 @@ public class ModuleIOSpark implements ModuleIO {
     turnController = turnSpark.getClosedLoopController();
 
     // Configure drive motor
-    var driveConfig = new SparkFlexConfig();
+    var driveConfig = new SparkMaxConfig();
     driveConfig
         .idleMode(IdleMode.kBrake)
         .smartCurrentLimit(driveMotorCurrentLimit)
