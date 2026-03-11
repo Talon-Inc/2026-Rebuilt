b555048ce763b8152e01e9cf95c655f30c024494
madpikachu97 <johnadare3@gmail.com>
2026-03-11 15:52:44 -0500

    Update Intake

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -4,13 +4,18 @@
 
 package frc.robot.subsystems.intake;
 
-
-import com.revrobotics.spark.SparkMax;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+<<<<<<< HEAD
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkAbsoluteEncoder;
+=======
+import com.revrobotics.spark.SparkMax;
+>>>>>>> 5bcba48bcbfb3f607df858e317035ecef4cc5476
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Configs.IntakeConfigs;
 import frc.robot.Constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
@@ -22,7 +27,13 @@ public class Intake extends SubsystemBase {
   public Intake() {
     deployMotor = new SparkMax(IntakeConstants.kDeployMotorId, MotorType.kBrushless);
     intakeMotor = new SparkMax(IntakeConstants.kIntakeMotorId, MotorType.kBrushless);
+<<<<<<< HEAD
     encoder = deployMotor.getAbsoluteEncoder();
+=======
+
+    deployMotor.configure(IntakeConfigs.deployConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    intakeMotor.configure(IntakeConfigs.intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+>>>>>>> 5bcba48bcbfb3f607df858e317035ecef4cc5476
   }
 
   // used to lower and raise intake
