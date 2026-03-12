eb075b4fec9031ecb66d0f6f2c5cdba7b052b807
JonathanLabrum <jonathan.d.labrum@gmail.com>
2026-03-12 14:34:16 -0500

    Create Agitate.java

---
+++ src/main/java/frc/robot/commands/Agitate.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intake.Intake;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class Agitate extends Command {
+  private final Intake intake;
+    
+  /** Creates a new Agitate. */
+  public Agitate(Intake intake) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.intake = intake;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    // starts by lowering the intake
+    intake.deploy(1);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double position = intake.getPosition(); // in degrees
+    if (position < 5) { 
+      intake.deploy(1);
+    }
+    else if (position > 60) {
+      intake.deploy(-1);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    // ends by raising the intake
+    intake.deploy(-1);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
